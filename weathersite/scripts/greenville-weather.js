67e6cbe2a2a2e7fec943dbbd2d97b3715c775b60
Lisa Parker <lisaparker@Lisas-MacBook-Pro.local>
2018-06-29 22:29:58 -0700

    Weather Site Update: Friday

---
+++ weathersite/scripts/greenville-weather.js
@@ -0,0 +1,25 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURL = 'https://api.openweathermap.org/data/2.5/weather?&id=4580543&units=imperial&APPID=e0d8e34fa7fdcca562398001c2708ca6';
+weatherRequest.open('GET', apiURL, true);
+weatherRequest.send();
+
+weatherRequest.onload = function() {
+  //console.log(weatherRequest.responseText);
+  
+  let weatherData = JSON.parse(weatherRequest.responseText);
+  console.log(weatherData);
+  
+  
+  document.getElementById('current-temp').innerHTML = Math.round(weatherData.main.temp);
+  
+  document.getElementById('current-conditions').innerHTML = weatherData.weather[0].description;
+  
+  document.getElementById('wind').innerHTML = Math.round(weatherData.wind.speed);
+  
+  
+  let imagesrc = 'https://openweathermap.org/img/w/' + weatherData.weather[0].icon + '.png';
+  
+  document.getElementById('weatherimage').src = imagesrc;
+}
+
+
