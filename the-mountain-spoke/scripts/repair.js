ec49c06adb2d4076a39461d9a96e7df06e1e62d2
Lisa Parker <lisaparker@Lisas-MacBook-Pro.local>
2018-07-24 15:57:26 -0700

    update chart

--- the-mountain-spoke/scripts/repair.js
+++ the-mountain-spoke/scripts/repair.js
@@ -1,17 +1,17 @@
-
-  
-var data = new XMLHttpRequest();
-var dataURL =  "";
+let data = new XMLHttpRequest();
+let dataURL =  "https://lisa-parker.github.io/the-mountain-spoke/data/chart.json";
 data.open('GET', dataURL, true);
 data.send();
 
+//console.log(data.responseText);
 
-  data.onload = function() {
+
+data.onload = function() {
   
-    var priceData = JSON.parse(data.responseText);
+  let priceData = JSON.parse(data.responseText);
   
-    for (i = 0; i < priceData.towns.length; i++) {
-      console.log(priceData);
+    for (i = 0; i < priceData.repairs.length; i++) {
+      //console.log(priceData);
     
       
       document.getElementById(priceData.repairs[i].name + "-name").innerHTML = priceData.repairs[i].name;
@@ -19,7 +19,6 @@ data.send();
       document.getElementById(priceData.repairs[i].name + "-repairs").innerHTML = priceData.repairs[i].repair;
       
       document.getElementById(priceData.repairs[i].name + "-maintain").innerHTML = priceData.repairs[i].maintain;
-      
     }
   
   }
