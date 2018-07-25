2922ffab769571cf6c09e524f6784cb3d377ebab
Lisa Parker <lisaparker@Lisas-MacBook-Pro.local>
2018-07-24 18:39:08 -0700

    json add data

--- the-mountain-spoke/scripts/repair.js
+++ the-mountain-spoke/scripts/repair.js
@@ -1,24 +1,21 @@
-let data = new XMLHttpRequest();
-let dataURL =  "https://lisa-parker.github.io/the-mountain-spoke/data/chart.json";
+var data = new XMLHttpRequest();
+var dataURL =  "https://lisa-parker.github.io/the-mountain-spoke/data/chart.json";
 data.open('GET', dataURL, true);
 data.send();
 
-//console.log(data.responseText);
 
-
-data.onload = function() {
+  data.onload = function() {
   
-  let priceData = JSON.parse(data.responseText);
+    var priceData = JSON.parse(data.responseText);
   
     for (i = 0; i < priceData.repairs.length; i++) {
-      console.log(priceData);
-    
       
-      document.getElementById(priceData.repairs[i].name + "-name").innerHTML = priceData.repairs[i].name;
+    //console.log(priceData);
       
-      document.getElementById(priceData.repairs[i].name + "-repairs").innerHTML = priceData.repairs[i].repair;
+    document.getElementById(priceData.repairs[i].name + "-name").innerHTML = priceData.repairs[i].name;    
       
-      document.getElementById(priceData.repairs[i].name + "-maintain").innerHTML = priceData.repairs[i].maintain;
-    }
-  
-  }
+    document.getElementById(priceData.repairs[i].name + "-repair").innerHTML = priceData.repairs[i].repair;
+      
+    document.getElementById(priceData.repairs[i].name + "-maintain").innerHTML = priceData.repairs[i].maintain;
+    } 
+}
